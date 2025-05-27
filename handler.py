37d67a97eeb15281d4ef22a02c6eb7884e27f14d
Joshua Anderton <info@joshanderton.com>
2025-05-26 22:19:30 -0700

    Use smaller test model, fix video output

--- handler.py
+++ handler.py
@@ -7,32 +7,18 @@ from PIL import Image
 import boto3
 import uuid
 from pathlib import Path
-# from huggingface_hub import login
-
 from diffusers import (
     AutoPipelineForImage2Image,
     AutoPipelineForText2Image,
-    WanPipeline
+    WanPipeline,
+    AutoencoderKLWan
+)
+from diffusers.utils import (
+    export_to_video, load_image
 )
 
 # Global model cache
 loaded_models = {}
-
-# Login to Hugging Face Hub just in case
-# login(token=os.environ["HUGGING_FACE_ACCESS_TOKEN"])
-
-def decode_image(b64_string):
-    image_data = base64.b64decode(b64_string)
-    return Image.open(io.BytesIO(image_data)).convert("RGB")
-
-def encode_image(pil_image):
-    buffered = io.BytesIO()
-    pil_image.save(buffered, format="PNG")
-    return base64.b64encode(buffered.getvalue()).decode("utf-8")
-
-def encode_video(video_path):
-    with open(video_path, "rb") as f:
-        return base64.b64encode(f.read()).decode("utf-8")
 
 def get_valid_kwargs(pipe, input_data):
     """Filter input_data to only the kwargs accepted by the pipeline."""
@@ -54,13 +40,18 @@ def upload_to_s3(file_path, file_type):
     return f"{url}/{bucket}/{key}"
 
 def handler(event):
-    task = event['input'].get('task')       # text-to-image, image-to-image, image-to-video
-    model_id = event['input'].get('model')  # e.g., runwayml/stable-diffusion-v1-5
-    prompt = event['input'].get('prompt')   # Prompt text
-    image_b64 = event['input'].get('image') # base64 image input (optional)
-    seed = event['input'].get('seed', -1)   # Random seed (optional)
     #kwargs = get_valid_kwargs(pipe, event["input"])
-    input_image_path = "input.png"
+    task = event['input'].get('task')                           # text-to-image, image-to-image, image-to-video
+    model_id = event['input'].get('model')                      # e.g., runwayml/stable-diffusion-v1-5
+    prompt = event['input'].get('prompt')                       # Prompt text
+    input_image = event['input'].get('image')                   # base64 image input (optional)
+    image_url = event['input'].get('image_url')                 # Image URL input (optional)
+    seed = event['input'].get('seed', -1)                       # Random seed (optional)
+    height = event['input'].get('height', 480)                  # Image height (optional)
+    width = event['input'].get('width', 832)                    # Image height (optional)
+    num_frames = event['input'].get('num_frames', 33)           # Number of frames for video generation (optional)
+    guidance_scale = event['input'].get('guidance_scale', 5.0)  # Guidance scale for generation (optional)
+    negative_prompt = event['input'].get('negative_prompt', "Bright tones, overexposed, static, blurred details, subtitles, style, works, paintings, images, static, overall gray, worst quality, low quality, JPEG compression residue, ugly, incomplete, extra fingers, poorly drawn hands, poorly drawn faces, deformed, disfigured, misshapen limbs, fused fingers, still picture, messy background, three legs, many people in the background, walking backwards")
 
     if not task or not model_id or not prompt:
         return {"error": "Missing required fields: 'task', 'model', or 'prompt'"}
@@ -85,10 +76,10 @@ def handler(event):
         return { "url": s3_url }
 
     elif task == "image-to-image":
-        if not image_b64:
-            return {"error": "Missing 'image' input for image-to-image task."}
-        input_image = decode_image(image_b64)
-        input_image.save(input_image_path)
+        if not image_url:
+            return {"error": "Missing 'image_url' input for image-to-video task."}
+
+        input_image = load_image(image_url)
 
         if model_id not in loaded_models:
             model = AutoPipelineForImage2Image.from_pretrained(model_id, torch_dtype=torch_dtype)
@@ -96,7 +87,7 @@ def handler(event):
             loaded_models[model_id] = model
 
         model = loaded_models[model_id]
-        output = model(prompt=prompt, image=input_image_path, strength=0.75, guidance_scale=7.5).images[0]
+        output = model(prompt=prompt, image=input_image, strength=0.75, guidance_scale=7.5).images[0]
 
         out_path = "output.png"
         output.save(out_path)
@@ -104,40 +95,68 @@ def handler(event):
 
         return { "url": s3_url }
 
-    elif task == "text-to-video" or task == "image-to-video":
+    elif task == "text-to-video":
         if model_id not in loaded_models:
+            vae = AutoencoderKLWan.from_pretrained(model_id, subfolder="vae", torch_dtype=torch.float32)
             if model_id.startswith("Wan-AI/"):
-                model = WanPipeline.from_pretrained(model_id, torch_dtype=torch_dtype)
+                model = WanPipeline.from_pretrained(model_id, vae=vae, torch_dtype=torch.bfloat16)
             else:
-                model = AutoPipelineForText2Image.from_pretrained(model_id, torch_dtype=torch_dtype)
+                model = AutoPipelineForText2Image.from_pretrained(model_id, vae=vae, torch_dtype=torch.bfloat16)
             model.to(device)
             loaded_models[model_id] = model
 
         model = loaded_models[model_id]
-        output = model(prompt=prompt, num_frames=16)
 
         out_path = "output.mp4"
-        output.save(out_path)
+        frames = pipe(
+            prompt=prompt,
+            negative_prompt=negative_prompt,
+            height=height,
+            width=width,
+            num_frames=num_frames,
+            guidance_scale=guidance_scale,
+        ).frames[0]
+        export_to_video(frames, out_path, fps=16)
         s3_url = upload_to_s3(out_path, "video/mp4")
 
         return { "url": s3_url }
 
     elif task == "image-to-video":
-        if not image_b64:
-            return {"error": "Missing 'image' input for image-to-video task."}
-        input_image = decode_image(image_b64)
-        input_image.save(input_image_path)
+
+        if not image_url:
+            return {"error": "Missing 'image_url' input for image-to-video task."}
+
+        input_image = load_image(image_url)
 
         if model_id not in loaded_models:
-            model = AutoPipelineForText2Image.from_pretrained(model_id, torch_dtype=torch_dtype)
+            vae = AutoencoderKLWan.from_pretrained(model_id, subfolder="vae", torch_dtype=torch.float32)
+            if model_id.startswith("Wan-AI/"):
+                model = WanPipeline.from_pretrained(model_id, vae=vae, torch_dtype=torch.bfloat16)
+            else:
+                model = AutoPipelineForText2Image.from_pretrained(model_id, vae=vae, torch_dtype=torch.bfloat16)
             model.to(device)
             loaded_models[model_id] = model
 
         model = loaded_models[model_id]
 
-        output = model(image=input_image_path, num_frames=16)
+        max_area = 480 * 832
+        aspect_ratio = image.height / image.width
+        mod_value = model.vae_scale_factor_spatial * model.transformer.config.patch_size[1]
+        height = round(np.sqrt(max_area * aspect_ratio)) // mod_value * mod_value
+        width = round(np.sqrt(max_area / aspect_ratio)) // mod_value * mod_value
+        image = image.resize((width, height))
+
+        frames = pipe(
+            image=image,
+            prompt=prompt,
+            negative_prompt=negative_prompt,
+            height=height,
+            width=width,
+            num_frames=num_frames,
+            guidance_scale=guidance_scale,
+        ).frames[0]
         output_path = "output.mp4"
-        output.save(output_path)
+        export_to_video(output, output_path, fps=16)
         s3_url = upload_to_s3(output_path, "video/mp4")
 
         return { "url": s3_url }
