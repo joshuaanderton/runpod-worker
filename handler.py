836fd6524ab3879887614879a735b6087faf2aab
Joshua Anderton <info@joshanderton.com>
2025-05-27 22:26:38 -0700

    Cleanup

--- handler.py
+++ handler.py
@@ -9,8 +9,8 @@ from pathlib import Path
 import boto3
 import os
 from diffusers import (
-    AutoPipelineForImage2Image,
     AutoPipelineForText2Image,
+    AutoPipelineForImage2Image,
     FluxPipeline,
     WanPipeline,
     AutoencoderKLWan
@@ -23,10 +23,28 @@ from diffusers.utils import (
 # Global model cache
 loaded_models = {}
 
-def get_valid_kwargs(pipe, input_data):
-    """Filter input_data to only the kwargs accepted by the pipeline."""
-    valid_keys = pipe.__call__.__code__.co_varnames
-    return {k: v for k, v in input_data.items() if k in valid_keys}
+def get_model(model_id, task):
+
+    if model_id in loaded_models:
+        return loaded_models[model_id]
+
+    if task.endswith("-image"):
+        if model_id.startswith("black-forest-labs/FLUX"):
+            model = FluxPipeline.from_pretrained(model_id, torch_dtype=torch_dtype)
+        elif task == "text-to-image":
+            model = AutoPipelineForText2Image.from_pretrained(model_id, torch_dtype=torch.bfloat16)
+        elif task == "image-to-image":
+            model = AutoPipelineForImage2Image.from_pretrained(model_id, torch_dtype=torch.bfloat16)
+
+    elif task.endswith("-video"):
+        vae = AutoencoderKLWan.from_pretrained(model_id, subfolder="vae", torch_dtype=torch.float32)
+        if model_id.startswith("Wan-AI/"):
+            model = WanPipeline.from_pretrained(model_id, vae=vae, torch_dtype=torch.bfloat16)
+        else:
+            model = AutoPipelineForText2Image.from_pretrained(model_id, vae=vae, torch_dtype=torch.bfloat16)
+
+    loaded_models[model_id] = model
+    return model
 
 def upload_to_cloud(file_path):
     bucket = os.environ["AWS_BUCKET"]
@@ -36,23 +54,22 @@ def upload_to_cloud(file_path):
     session = boto3.session.Session()
     client = session.client(
         's3',
-        region_name=os.environ["AWS_REGION"],
-        endpoint_url=os.environ["AWS_ENDPOINT_URL"],
-        aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
-        aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
+        region_name=os.environ.get("AWS_REGION", None),
+        endpoint_url=os.environ.get("AWS_ENDPOINT_URL", None),
+        aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID", None),
+        aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY", None)
     )
 
     # Upload the file
     response = client.upload_file(file_path, bucket, key)
 
-    return f"{os.environ["AWS_URL"]}/{bucket}/{key}"
+    return f"{os.environ['AWS_URL']}/{bucket}/{key}"
 
 def handler(event):
-    #kwargs = get_valid_kwargs(pipe, event["input"])
     task = event['input'].get('task')                           # text-to-image, image-to-image, image-to-video
     model_id = event['input'].get('model')                      # e.g., runwayml/stable-diffusion-v1-5
+
     prompt = event['input'].get('prompt')                       # Prompt text
-    input_image = event['input'].get('image')                   # base64 image input (optional)
     image_url = event['input'].get('image_url')                 # Image URL input (optional)
     seed = event['input'].get('seed', -1)                       # Random seed (optional)
     height = event['input'].get('height', 480)                  # Image height (optional)
@@ -62,65 +79,29 @@ def handler(event):
     negative_prompt = event['input'].get('negative_prompt', "Bright tones, overexposed, static, blurred details, subtitles, style, works, paintings, images, static, overall gray, worst quality, low quality, JPEG compression residue, ugly, incomplete, extra fingers, poorly drawn hands, poorly drawn faces, deformed, disfigured, misshapen limbs, fused fingers, still picture, messy background, three legs, many people in the background, walking backwards")
 
     output_name = uuid.uuid4()
+    output_path = f"{output_name}.png"
 
     if not task or not model_id or not prompt:
         return { "error": "Missing required fields: 'task', 'model', or 'prompt'" }
 
-    # Setup device
-    device = torch.device("cuda")
-    torch_dtype = torch.float16
+    torch_dtype = torch.bfloat16
+    model = get_model(model_id, task)
+    model.to(torch.device("cuda"))
 
     if task == "text-to-image":
-        if model_id not in loaded_models:
-            if model_id.startswith("black-forest-labs/FLUX"):
-                model = FluxPipeline.from_pretrained(model_id, torch_dtype=torch_dtype)
-            else:
-                model = AutoPipelineForText2Image.from_pretrained(model_id, torch_dtype=torch_dtype)
-            model.to(device)
-            loaded_models[model_id] = model
-
-        model = loaded_models[model_id]
-        output = model(prompt).images[0]
-
         output_path = f"{output_name}.png"
-        output.save(output_path)
-        s3_url = upload_to_cloud(output_path, "image/png")
-
-        return { "url": s3_url }
+        model(prompt).images[0].save(output_path)
 
     elif task == "image-to-image":
         if not image_url:
             return { "error": "Missing 'image_url' input for image-to-video task." }
 
         input_image = load_image(image_url)
-
-        if model_id not in loaded_models:
-            model = AutoPipelineForImage2Image.from_pretrained(model_id, torch_dtype=torch_dtype)
-            model.to(device)
-            loaded_models[model_id] = model
-
-        model = loaded_models[model_id]
         output = model(prompt=prompt, image=input_image, strength=0.75, guidance_scale=7.5).images[0]
-
-        output_path = f"{output_name}.png"
         output.save(output_path)
-        s3_url = upload_to_cloud(output_path, "image/png")
-
-        return { "url": s3_url }
 
     elif task == "text-to-video":
-        if model_id not in loaded_models:
-            vae = AutoencoderKLWan.from_pretrained(model_id, subfolder="vae", torch_dtype=torch.float32)
-            if model_id.startswith("Wan-AI/"):
-                model = WanPipeline.from_pretrained(model_id, vae=vae, torch_dtype=torch.bfloat16)
-            else:
-                model = AutoPipelineForText2Image.from_pretrained(model_id, vae=vae, torch_dtype=torch.bfloat16)
-            model.to(device)
-            loaded_models[model_id] = model
 
-        model = loaded_models[model_id]
-
-        output_path = f"{output_name}.mp4"
         frames = model(
             prompt=prompt,
             negative_prompt=negative_prompt,
@@ -129,10 +110,9 @@ def handler(event):
             num_frames=num_frames,
             guidance_scale=guidance_scale,
         ).frames[0]
-        export_to_video(frames, output_path, fps=16)
-        s3_url = upload_to_cloud(output_path, "video/mp4")
 
-        return { "url": s3_url }
+        output_path = "{output_name}.mp4"
+        export_to_video(output, output_path, fps=16)
 
     elif task == "image-to-video":
 
@@ -140,27 +120,15 @@ def handler(event):
             return { "error": "Missing 'image_url' input for image-to-video task." }
 
         input_image = load_image(image_url)
-
-        if model_id not in loaded_models:
-            vae = AutoencoderKLWan.from_pretrained(model_id, subfolder="vae", torch_dtype=torch.float32)
-            if model_id.startswith("Wan-AI/"):
-                model = WanPipeline.from_pretrained(model_id, vae=vae, torch_dtype=torch.bfloat16)
-            else:
-                model = AutoPipelineForText2Image.from_pretrained(model_id, vae=vae, torch_dtype=torch.bfloat16)
-            model.to(device)
-            loaded_models[model_id] = model
-
-        model = loaded_models[model_id]
-
         max_area = 480 * 832
-        aspect_ratio = image.height / image.width
+        aspect_ratio = input_image.height / input_image.width
         mod_value = model.vae_scale_factor_spatial * model.transformer.config.patch_size[1]
         height = round(np.sqrt(max_area * aspect_ratio)) // mod_value * mod_value
         width = round(np.sqrt(max_area / aspect_ratio)) // mod_value * mod_value
-        image = image.resize((width, height))
+        image = input_image.resize((width, height))
 
         frames = model(
-            image=image,
+            image=input_image,
             prompt=prompt,
             negative_prompt=negative_prompt,
             height=height,
@@ -168,14 +136,12 @@ def handler(event):
             num_frames=num_frames,
             guidance_scale=guidance_scale,
         ).frames[0]
+
         output_path = "{output_name}.mp4"
         export_to_video(output, output_path, fps=16)
-        s3_url = upload_to_cloud(output_path, "video/mp4")
 
-        return { "url": s3_url }
-
-    else:
-        return { "error": f"Unsupported task: {task}" }
+    url = upload_to_cloud(output_path)
+    return { "url": url }
 
 # Start the Serverless function when the script is run
 if __name__ == '__main__':
